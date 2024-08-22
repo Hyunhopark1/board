cb6513508ce2e31ff272ba4f89b935a8cbb1e1e1
Hyunhopark1 <phh1569@nate.com>
2024-08-22 12:21:09 +0900

    add

--- plannerproject/src/main/java/com/phh/plannerproject/board/IBoardService.java
+++ plannerproject/src/main/java/com/phh/plannerproject/board/IBoardService.java
@@ -3,6 +3,14 @@ package com.phh.plannerproject.board;
 public interface IBoardService {
 
 
+    IBoard insert(BoardDto dto);
+
+    IBoard update(BoardDto dto);
+
+    IBoard findById(Long id);
+
+    Boolean delete(Long id);
+
 
 
 }
